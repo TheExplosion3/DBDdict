60ab66301302e47b41c1e2c3e754ee2e83d36568
TheExplosion3 <GOwl2515@gmail.com>
2022-05-03 17:38:55 -0400

    idk im still testing lmao

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -81,7 +81,7 @@ int main() {
             userInputUS = 0;
             continue;
           }
-          
+          // verifies that user input for the addon isnt the same as the first addon already entered
           while(true) {
            
             if(lowercaseString(fObject.getAddOn(0).getName()).compare(userInputS) == 0) {
